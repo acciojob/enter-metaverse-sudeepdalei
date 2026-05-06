e80940b185f346cdbe4055dd196e27ee1751831a
Sudeep Kumar Dalei <sudeepdalei38@gmail.com>
2026-05-06 19:43:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const status = document.getElementById("status");
+const enterBtn = document.getElementById("enterBtn");
+
+enterBtn.addEventListener("click", function () {
+    status.innerHTML = "<h1>Entered Metaverse</h1>";
+});
